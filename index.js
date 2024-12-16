d4985d791fa877d98f4d0b5e0dcf95e1c6bd1a00
humblehustler94 <flores.itzel94@gmail.com>
2024-12-16 10:16:44 -0800

    Refactor - GET requests
    Refactor GET requests for my project.
    Add a list of my favorite movies :
    let topMovies = [ ];
    
    Add middleware to project.

--- index.js
+++ index.js
@@ -1,35 +1,85 @@
 const express = require('express');
 const app = express();
 
+// ADD middleware 
+let myLogger = (req, res, next) => {
+    console.log(req.url);
+    next();
+};
+
+let requestTime = (req, res, next => {
+    req.requestTime = Date.now();
+    next();
+});
+
+// ADD 
+app.use(myLogger);
+app.use(requestTime);
+
 // code added in section 2.4
 // benefit of using Express is that simplifies the Node.js syntax
-let topBooks = [
+// a list of my favorite movies : add 10 movies
+let topMovies = [
+
     {
-        title: 'Harry Potter and the Sorcerer\'s Stone',
-        author: 'J.K Rowling'
+        title: 'Instructions Not Included',
+        year: '2013'
     },
     {
-        title: 'Lord of the Rings',
-        author: 'J.R.R Tolkien'
+        title: 'Mighty Joe Young',
+        year: '1998'
     },
     {
-        title: 'Twilight',
-        author: 'Stephanie Meyer'
-    }
+        title: 'The Iron Giant',
+        year: '1999'
+    },
+    {
+        title: 'Fast Five',
+        year: '2011'
+    },
+    {
+        title: 'The Breakfast Club',
+        year: '1985'
+    },
+    {
+        title: 'The Sandlot',
+        year: '1993'
+    },
+    {
+        title: 'Blade',
+        year: '1998'
+    },
+    {
+        title: 'Edward Scissorhands',
+        year: '1990'
+    },
+    {
+        title: 'Transformers',
+        year: '2007'
+    },
+    {
+        title: 'Grease',
+        year: '1978'
+    },
+
 ];
 
-// GET requests --> 2.4
-// root request in project
-app.get('/', (req,res) => {
-    res.send('Welcome to my book club!'):
+
+// GET root requests in project --> 2.4
+app.get('/', (req, res) => {
+    res.send('Welcome to my app!');
 });
 
 app.get('/documentation', (req, res) => {
-    res.sendFile('public/documentation.html', { root: __dirname});
+    res.sendFile('public/documentation.html', { root: __dirname });
 });
 
-app.get('/books', (req, res) => {
-    res.json(topBooks);
+app.get('/secreturl', (req, res) => {
+    res.send('This is a secret url with super top-secret content.');
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
 });
 
 // listen for requests
