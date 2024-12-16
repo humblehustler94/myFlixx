3a2a53b671209b544e30c8f3f1b77896766d430d
humblehustler94 <flores.itzel94@gmail.com>
2024-12-16 09:49:09 -0800

    Refactor - Express
    Refactor code to set up for express.

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 const express = require('express');
 const app = express();
+
+// code added in section 2.4
+// benefit of using Express is that simplifies the Node.js syntax
+let topBooks = [
+    {
+        title: 'Harry Potter and the Sorcerer\'s Stone',
+        author: 'J.K Rowling'
+    },
+    {
+        title: 'Lord of the Rings',
+        author: 'J.R.R Tolkien'
+    },
+    {
+        title: 'Twilight',
+        author: 'Stephanie Meyer'
+    }
+];
+
+// GET requests --> 2.4
+// root request in project
+app.get('/', (req,res) => {
+    res.send('Welcome to my book club!'):
+});
+
+app.get('/documentation', (req, res) => {
+    res.sendFile('public/documentation.html', { root: __dirname});
+});
+
+app.get('/books', (req, res) => {
+    res.json(topBooks);
+});
+
+// listen for requests
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
